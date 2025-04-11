416574ef5c9db4015cd99bf13aaabcce813ec5e2
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2025-04-10 22:24:26 -0400

    added getCalendarEvents

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -78,4 +78,50 @@ module.exports.getAccessToken = async (e) => {
                 body: JSON.stringify(error),
             };
         });
-}; 
+};
+
+
+
+module.exports.getCalendarEvents = async (e) => {
+    //Decode authorization code extracted from the URL query
+    const access_token = decodeURIComponent(`${e.pathParamaters.access_token}`);
+    oAuth2Client.setCredentials({ access_token });
+
+    return new Promise((resolve, reject) => {
+
+        calendar.events.list(
+            {
+                calendarId: CALENDAR_ID,
+                auth: oAuth2Client,
+                timeMin: new Date().toISOString(),
+                singleEvents: true,
+                orderBY: "startTime",
+            },
+            (error, response) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(response);
+                }
+            }
+        );
+    })
+        .then((results) => {
+            //Respond with OAuth token
+            return {
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+                },
+                body: JSON.stringify({ events: results.data.items }),
+            };
+        })
+        .catch((error) => {
+            //Handle error
+            return {
+                statusCode: 500,
+                body: JSON.stringify(error),
+            };
+        });
+};
