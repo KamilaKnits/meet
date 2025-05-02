bb005f031689edeb8142205b025b69b5089ae238
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2025-05-01 22:02:27 -0400

    integration tests for Feature 1 and integrated calendar api

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,15 +1,16 @@
 //src/__test__/CitySearch.test.js
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
+import App from '../App';
 import { extractLocations, getEvents } from '../api';
 
 describe('<CitySearch /> component', () => {
     let CitySearchComponent;
     beforeEach(() => {
-        CitySearchComponent = render(<CitySearch />);
-    })
+        CitySearchComponent = render(<CitySearch allLocations={[]}/>);
+    });
 
     test('renders text input', () => {
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -36,7 +37,7 @@ describe('<CitySearch /> component', () => {
         const user = userEvent.setup();
         const allEvents = await getEvents();
         const allLocations = extractLocations(allEvents);
-        CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+        CitySearchComponent.rerender(<CitySearch allLocations={allLocations}  />);
 
 
         // user types "Berlin" in city textbox
@@ -60,7 +61,7 @@ describe('<CitySearch /> component', () => {
         const user = userEvent.setup();
         const allEvents = await getEvents();
         const allLocations = extractLocations(allEvents);
-        CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+        CitySearchComponent.rerender(<CitySearch allLocations={allLocations}setCurrentCity={() => { }} />);
 
 
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -69,13 +70,29 @@ describe('<CitySearch /> component', () => {
 
         // the suggestion's textContent look like this: "Berlin, Germany"
         const BerlinGermanySuggestion = CitySearchComponent.queryAllByRole('listitem')[0];
-
-
         await user.click(BerlinGermanySuggestion);
 
 
         expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
     });
+});
 
+describe('<CitySearch /> integration', () => {
+    test('renders suggestions list when the app is rendered.', async () => {
+        const user = userEvent.setup();
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
 
+        const CitySearchDOM = AppDOM.querySelector('#city-search');
+        const cityTextBox = within(CitySearchDOM).queryByRole("textbox");
+        await user.click(cityTextBox);
+
+        const allEvents = await getEvents();
+        const allLocations = extractLocations(allEvents);
+
+        await waitFor(() => {
+        const suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem');
+        expect(suggestionListItems.length).toBe(allLocations.length + 1);
+        });         
+    });
 });
