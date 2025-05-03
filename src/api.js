1e428d38e04dd7eb4eb015d296d41213f19025b2
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2025-05-03 13:32:44 -0400

    changed order of functions

--- src/api.js
+++ src/api.js
@@ -2,30 +2,25 @@
 import mockData from './mock-data';
 
 
-// /**
-//  *
-//  * @param {*} events:
-//  * The following function should be in the “api.js” file.
-//  * This function takes an events array, then uses map to create a new array with only locations.
-//  * It will also remove all duplicates by creating another new array using the spread operator 
-// and spreading a Set.
-//  * The Set will remove all duplicates from the array.
-//  */
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
+
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
   const locations = [...new Set(extractedLocations)];
   return locations;
 };
 
-const checkToken = async (accessToken) => {
-  const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
-  const result = await response.json();
-  return result;
-};
-
 //   This function will fetch the list of all events
 
 export const getEvents = async () => {
+
   if (window.location.href.startsWith('http://localhost')) {
     return mockData;
   }
@@ -42,6 +37,33 @@ export const getEvents = async () => {
     } else return null;
   }
 };
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch("https://s3i8xc2hxj.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url");
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
+
+
+const checkToken = async (accessToken) => {
+  const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
+  const result = await response.json();
+  return result;
+};
+
 
 const removeQuery = () => {
   let newurl;
@@ -69,21 +91,3 @@ const getToken = async (code) => {
   return access_token;
 };
 
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const response = await fetch("https://s3i8xc2hxj.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url");
-      const result = await response.json();
-      const { authUrl } = result;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
-};
