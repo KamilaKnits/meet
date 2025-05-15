949cc7b53792c6ea7d32101f4739711bfa9f8c8a
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2025-05-14 20:22:28 -0400

    added atatus-spa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import * as atatus from 'atatus-spa';
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+
+atatus.config('e8bff797ce8f4159b631e3178a06278a').install();
+
+// atatus.notify(new Error('Test Atatus Setup'));
