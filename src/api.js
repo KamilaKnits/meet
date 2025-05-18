2b3f5e452b8325201faabd39cf89b2cceabac933
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2025-05-18 15:11:52 -0400

    service worker added

--- src/api.js
+++ src/api.js
@@ -21,7 +21,11 @@ export const extractLocations = (events) => {
 //   This function will fetch the list of all events
 
 export const getEvents = async () => {
-
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events?JSON.parse(events):[];
+  }
 
   if (window.location.href.startsWith('http://localhost')) {
  
@@ -36,6 +40,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
